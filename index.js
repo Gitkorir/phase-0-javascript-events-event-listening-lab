b6c47858a297f41e4f947add4fc68022243dfdce
Arnold <arnoldkorir201@gmail.com>
2025-03-19 12:15:21 +0300

    addEventListener

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 function addingEventListener() {
+  const input = document.getElementById("button");
+  input.addEventListener("click", (e) => {
+    console.log("button clicked!");
+  });
+}
+if (typeof module !== "undefined" || module.exports) {
+  module.exports = {
+    addingEventListener,
+  };
 }
